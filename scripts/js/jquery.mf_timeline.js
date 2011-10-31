4f20b06ae8193b9f0c3b9d12c710c84dffb1204f
matt_d_rat <matt_d_rat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-31 01:46:05 +0000

    Initial release of MF Timeline.
    - Supports content from Wordpress and Twitter.
    - Wordpress content can be filtered by multiple post types and taxonomies (including custom).
    - Twitter content can be filtered by multiple hashtags.
    
    git-svn-id: http://plugins.svn.wordpress.org/wp-facebook-timeline-mf-timeline/trunk@457564 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scripts/js/jquery.mf_timeline.js
+++ scripts/js/jquery.mf_timeline.js
@@ -1,14 +1,12 @@
-jQuery(document).ready(function(){
-	if(jQuery('.timeline .timeline_nav')) {
-		jQuery('.timeline .timeline_nav').stickyfloat({duration: 500});
-	}
+$(document).ready(function(){
+	$('.timeline .timeline_nav').stickyfloat({duration: 500});
 	
-	jQuery('.timeline .section').each(function(){
-		jQuery(this).afterScroll(function(){
+	$('.timeline .section').each(function(){
+		$(this).afterScroll(function(){
 			// After we have scolled past the top
-			var year = jQuery(this).attr('id');
-			jQuery('ol.timeline_nav li').removeClass('current');
-			jQuery('ol.timeline_nav li#menu_year_' + year).addClass('current');
+			var year = $(this).attr('id');
+			$('ol.timeline_nav li').removeClass('current');
+			$('ol.timeline_nav li#menu_year_' + year).addClass('current');
 		});
 	});
 });

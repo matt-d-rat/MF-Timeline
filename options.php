82fbef9b65e9a93ba02c1ecc6fbdef12c3783ef5
matt_d_rat <matt_d_rat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-31 01:08:50 +0000

    Initial release of MF Timeline.
    - Supports content from Wordpress and Twitter.
    - Wordpress content can be filtered by multiple post types and taxonomies (including custom).
    - Twitter content can be filtered by multiple hashtags.
    
    git-svn-id: http://plugins.svn.wordpress.org/wp-facebook-timeline-mf-timeline/trunk@457494 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ options.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: WP Facebook Timeline (MF-Timeline)
+Plugin URI: http://www.aplaceformyhead.co.uk/mf-timeline
+Description: Creates a visual linear timeline representation from your Wordpress posts and other media sources in the style of Facebook Profile Timeline.
+Version: 1.0
+Author: Matt Fairbrass
+Author URI: http://www.aplaceformyhead.co.uk
+License: GPLv2
+.
+By default the timeline is styled to resemble Facebook's Profile Timeline, but you are free to override the enqueued plugin styles with your own.
+.
+*/
+require('class-mf-timeline.php');
+
+$mf_timeline = new MF_Timeline();
+?>
